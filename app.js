4b22bf46d961f8a3fdf2e9e34f889d950107726a
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-14 19:48:34 +0330

    Path Module

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const log = require("./logger"); // or require("./logger.js");
+// Built-in modules and global objects in node js: https://nodejs.org/docs/latest/api/
 
-console.log(log);
-log("Hello world!");
+// path module docs: https://nodejs.org/docs/latest/api/path.html
+
+const path = require("node:path");
+
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+// output:
+//{
+//   root: 'C:\\',
+//   dir: 'C:\\Users\\LENOVO\\source\\repos\\node-js-playground',
+//   base: 'app.js',
+//   ext: '.js',
+//   name: 'app'
+// }
