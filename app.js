4479e6849a66e82588f1b648bc684a5067ff1c4e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-14 20:03:08 +0330

    OS Module

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Built-in modules and global objects in node js: https://nodejs.org/docs/latest/api/
 
-// path module docs: https://nodejs.org/docs/latest/api/path.html
+// OS module docs: https://nodejs.org/docs/latest/api/os.html
 
-const path = require("node:path");
+// this file is a Common Js module!
 
-const pathObj = path.parse(__filename);
-console.log(pathObj);
-// output:
-//{
-//   root: 'C:\\',
-//   dir: 'C:\\Users\\LENOVO\\source\\repos\\node-js-playground',
-//   base: 'app.js',
-//   ext: '.js',
-//   name: 'app'
-// }
+const os = require("node:os");
+
+const totalMemory = os.totalmem(); // Returns the amount of free system memory in bytes as an integer.
+
+const freeMemory = os.freemem(); // Returns the total amount of system memory in bytes as an integer.
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+// Before Node we couldn't get this kind of information about OS because JS only work inside of browsers and worked with object like window or document.
