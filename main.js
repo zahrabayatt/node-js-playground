e2c0e71e1c4a5d5c9df9e58049212c41bb596c52
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-14 14:08:49 +0330

    What is Node

---
+++ main.js
@@ -0,0 +1,5 @@
+// Node js is an open source and cross platform runtime environment for executing JavaScript Code outside of a browser.
+
+// Quite often we use Node to build back-end services also called APIs (application Programming Interface).
+
+// Node is ideal for building highly scalable, data-intensive and real-time back-end services that power our client application.
