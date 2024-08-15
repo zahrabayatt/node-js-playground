c6819a4ebc7d71bcb70dc626a70da58ee7aa1602
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-15 17:57:37 +0330

    HTTP Module - create a simple server

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const Logger = require("./logger"); // uppercase naming cause it's a class
-const logger = new Logger();
+// Built-in modules and global objects in node js: https://nodejs.org/docs/latest/api/
 
-// Register a listener
-logger.on("messageLogged", (arg) => {
-  console.log("Listener called", arg);
+// this file is a Common Js module!
+
+// HTTP module docs: https://nodejs.org/docs/latest/api/http.html
+
+// we use HTTP module for creating networking applications. for example we can create a web server that listens for HTTP requests on a given port.
+
+// with this module we can easily create a back-end service for our client applications like a web applications that we build with react or angular or mobile application running on a mobile device.
+
+const http = require("node:http");
+
+const server = http.createServer(); // create a web server
+// this server is an event emitter so it has all the capabilities of event emitter. like we have a on method or addListener method,..
+server.listen(3000);
+
+server.on("connection", (socket) => {
+  console.log("New connection.");
 });
 
-logger.log("message");
-
-// output:
-// message
-// Listener called { id: 1, url: 'http://' }
+console.log("Listening on port 3000...");
