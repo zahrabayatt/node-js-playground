7db20bad698f7b5aff6003c67a5ad2cf3afc5807
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-15 17:46:02 +0330

    Extending EventEmitter

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-// Built-in modules and global objects in node js: https://nodejs.org/docs/latest/api/
+const Logger = require("./logger"); // uppercase naming cause it's a class
+const logger = new Logger();
 
-// this file is a Common Js module!
-
-// Events module docs: https://nodejs.org/docs/latest/api/events.html
-
-// Event is signal that indicates that something has happened in our application.
-
-// emit means make a noise or produce something!
-
-const EventEmitter = require("node:events"); // the naming is uppercase because it's a class not function or simple variable!
-
-// class is a container for a bunch of related methods and properties.
-
-const emitter = new EventEmitter(); // emitter is a object.
-
-// Register a Listener - receive event argument
-emitter.on("messageLogged", (arg) => {
+// Register a listener
+logger.on("messageLogged", (arg) => {
   console.log("Listener called", arg);
 });
 
-// Raise a Event with event argument
-emitter.emit("messageLogged", { id: 1, url: "http://" });
+logger.log("message");
+
+// output:
+// message
+// Listener called { id: 1, url: 'http://' }
