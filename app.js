b88c11fc0290945ae6f8f783f4831876a4e449bd
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-15 18:20:00 +0330

    Package.json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// NPM or node package manager is a command line tool as well as a registry of third party library that we can to our node application. so pretty much for any kind of functionality there is a free and open-source npm module in npm registry that we can add tor our node application.
+// package.json is basically is a json file with basic information (meta data) about your application and all node application by standard has this package.json file.
 
-// if you have a ideo for a node module or functionality that you want to share with others, you can create your own node module and publish it in npm. and others can add this module via a command line call npm to their node application.
-
-// https://www.npmjs.com/
+// to create package.json, run this command:
+// npm init
+// or
+// npm init - y
