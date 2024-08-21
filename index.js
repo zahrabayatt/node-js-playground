8fa5a90adda2897ad550a794c8d308f4aba0dfe6
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-22 00:23:47 +0330

    Uninstall a Package

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-// some node package in our application are used only during development for example packages for running unit tests, analysis code, bundling js and these dependencies are development dependencies and they not should go in production development where we deploy our application.
-
-// To Install a package as dev dependency, run this command:
-// npm i [package name] --save-dev
-// Or
-// npm i [package name] -D
-
-// All of the dependencies Whether is a application's dependency or a development dependency are installed and located under the node_module folder.
+// To uninstall a node package, run this command:
+// npm un [package name]
