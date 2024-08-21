2c2bde13d3581451245f067da428f271d9bc1cb9
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-22 00:22:16 +0330

    DevDependencies

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// To update version of a published node package you con run this command:
-// npm version minor (update minor version)
+// some node package in our application are used only during development for example packages for running unit tests, analysis code, bundling js and these dependencies are development dependencies and they not should go in production development where we deploy our application.
 
-// To publish of a node package, you can run this command:
-// npm publish
+// To Install a package as dev dependency, run this command:
+// npm i [package name] --save-dev
+// Or
+// npm i [package name] -D
+
+// All of the dependencies Whether is a application's dependency or a development dependency are installed and located under the node_module folder.
