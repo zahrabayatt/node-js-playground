20b99cfd678475f5be576c31316af16616ae7fd1
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-22 00:31:29 +0330

    Working with Global Packages

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-// To uninstall a node package, run this command:
-// npm un [package name]
+// To install a package globally, run this command:
+// npm i -g [package name]
+
+// To see all global outdated packages, run this command:
+// npm -g outdated
