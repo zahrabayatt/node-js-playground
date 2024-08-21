d5de8d8f13cd78193e073836460b236b4cd3d3a4
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-21 23:58:49 +0330

    Viewing Registry Info for a Package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// To check what version of dependency is installed, you can check package.json for the package in the node_modules folder or you can run this command to see list of all dependencies with exact version:
+// to check dependencies of a npm package you can check the Dependencies section in npmjs.com about that package or you can run these command:
 
-// npm list or npm list--depth=0 : this command show only application's dependencies
+// npm view [package name] - see package.json of npm package
+// npm view [package name] dependencies - see only dependencies of npm package
 
-// npm list--depth=1 : this command show application's dependencies and dependencies of each of those applications
+// npm view [package name] versions - see all versions of npm package
