a88e109f34fbf715168c469b687bdefaad5f66df
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-22 15:38:08 +0330

    Updating Local Packages

---
+++ index.js
@@ -0,0 +1,13 @@
+// To check what packages are updated in your application, run this command: npm outdated
+
+// To update a local package:
+// npm update --> only work for minor and patch releases
+
+// To update a update to major release using npm-check-updates command line:
+// npm-check-updates -u
+// or
+// ncu - u
+// After than we should run this to install updated package:
+// npm i
+
+// to install npm-check-updates run: npm i -g npm-check-updates
--- node-app/index.js
+++
@@ -1,3 +0,0 @@
-const lion = require("lion-lib-203947");
-const result = lion.add(3, 4);
-console.log(result);
