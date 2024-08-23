9197d3942fd543a1bd6432dcd69ea2d8da628f14
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-23 15:19:18 +0330

    Route and Query String Parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,19 +10,25 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-// Read PORT from environment variables
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  // res.send(req.params.year + " " + req.params.month);
+  // res.send(req.params);
+  res.send(req.query);
+  //res.send(req.query.sortBy);
+});
+
+// we use route parameters for essentials or required values and we use query string parameters to provide additional data and anything is optional
+
+// read route parameters from req in handler: req.params
+// read query string parameters from req in handler: req.query
+
+// we define query strings parameters in endpoint like this:
+// ?sortBy=name
+// ?sortBy=name&idFilter=2
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-// Define a environment variable in Powershell:
-// $env:PORT=5000
-// Check the value of environment variable in Powershell:
-// $env:PORT
-
-// Define a environment variable in cmd:
-// set PORT=5000
-// Check the value of environment variable in cmd:
-// set PORT
-
-// the both way is temporary and exist until you close terminal.
-// to define a permeate environment variable in windows machine check this link: https://stackoverflow.com/questions/5898131/set-a-persistent-environment-variable-from-cmd-exe
