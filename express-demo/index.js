0a89c72e35ab5cc6a30fff888beecb695f86e5fa
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-23 15:26:45 +0330

    Handling HTTP GET Requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,34 +1,29 @@
 const express = require("express");
-
 const app = express();
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found!");
+  }
+
+  res.send(course);
 });
-
-app.get("/api/posts/:year/:month", (req, res) => {
-  // res.send(req.params.year + " " + req.params.month);
-  // res.send(req.params);
-  res.send(req.query);
-  //res.send(req.query.sortBy);
-});
-
-// we use route parameters for essentials or required values and we use query string parameters to provide additional data and anything is optional
-
-// read route parameters from req in handler: req.params
-// read query string parameters from req in handler: req.query
-
-// we define query strings parameters in endpoint like this:
-// ?sortBy=name
-// ?sortBy=name&idFilter=2
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
