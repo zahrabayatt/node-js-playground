940445a9dd3a4045f8c3d5c41ff89a27ef8f56a0
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-23 15:31:09 +0330

    Handling HTTP POST Requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Add middleware to get req.body
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -22,6 +25,16 @@ app.get("/api/courses/:id", (req, res) => {
     res.status(404).send("The course with the given ID was not found!");
   }
 
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
   res.send(course);
 });
 
