b266026caa9f7be0fdcf975215a6f3e584b12e51
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-03 22:20:12 +0330

    What is Middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,7 @@
-const Joi = require("joi"); // it return a class, so the first character of module's name should be Capital
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
-// Add middleware to get req.body
 app.use(express.json());
 
 const courses = [
@@ -84,3 +83,22 @@ app.delete("/api/courses/:id", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Middleware Or Middleware Function:
+// It's basically a function that take a request object and either returns a response to the client or passes control to another middleware function.
+
+// Example:
+// route handler like in app.get(route, handler), handle is a middleware function.
+
+// express.json(); it returns a middleware function and the job of it to read a request and if there is a json object in the body of the request, it will parsed the body of the request into the json object and then will set req.body property in request
+
+// Request Processing Pipeline:
+// In runtime when we receive a request on the server, that request goes through this pipeline. in this pipeline we have one or more middleware function. each middleware function either determines the request's response cycle by returning response object or it will pass control to another middleware function.
+// in our current implementation our request processing pipeline has two middleware function:
+// the first one is json() which parses the json object of request body in req.body and passes control to second middleware function which is route() handler and it determines response object.
+
+// Express include a few built-in middleware function but we can also define our custom middleware functions and put it in front of our request processing pipeline.
+
+// every request we get on the server will go through of middleware functions.
+
+// with custom middleware functions we can do cost cutting concerns like login, authentication, authorization,...
