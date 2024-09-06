8ca736cc71c9ce2d843fcd5c213975fed9f04173
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-06 20:30:22 +0330

    Named Functions to Reduce Callback Hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -3,10 +3,25 @@ console.log("Before");
 // with callbacks, our code will be end up like this if all functions are asynchronous, the syntax is nested and refers to this as callback hell:
 getUser(1, (user) => {
   getRepositories(user.githubUsername, (repos) => {
-    getCommies(repo, (commits) => {});
+    getCommies(repos, (commits) => {});
   });
 });
+
+// To solve this problem, we are going to replace each anonymous function with a named function and with this we can flatten the structure of this code:
+getUser(1, getRepositories);
+
 console.log("After");
+function getRepositories(user) {
+  getRepositories(user.githubUsername, getCommies);
+}
+
+function getCommies(repos) {
+  getCommies(repos, displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log(commits);
+}
 
 function getUser(id, callback) {
   setTimeout(() => {
