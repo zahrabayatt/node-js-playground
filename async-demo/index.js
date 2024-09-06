b4673a1f29d246f7d3dff49461405e2435532d0d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-07 02:12:41 +0330

    Async and Await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,18 +1,35 @@
 console.log("Before");
 
-getUser(1)
-  .then((user) => getRepositories(user.githubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log(commits))
-  .catch((err) => console.log("Error", err.message));
+// Promise-based approach
+// getUser(1)
+//   .then((user) => getRepositories(user.githubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log(commits))
+//   .catch((err) => console.log("Error", err.message));
+
+// Async and Await approach
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repo = await getRepositories(user.githubUsername);
+    const commit = await getCommits(repo[0]);
+    console.log(commit);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+
+displayCommits();
 
 console.log("After");
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("Reading a use from a database...");
+      console.log("Reading a user from a database...");
       resolve({ id: id, githubUsername: "zahrabayatt" });
+      // reject(new Error("Couldn't get the user "));
     }, 2000);
   });
 }
