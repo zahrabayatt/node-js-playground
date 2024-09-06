3ac4d1db22975147393303f9160da96410930fc6
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-07 01:38:35 +0330

    Replacing Callbacks with Promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,24 +1,15 @@
+// async with callbacks:
+
 console.log("Before");
 getUser(1, (user) => {
   getRepositories(user.githubUsername, (repos) => {
-    getCommies(repos, (commits) => {});
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
   });
 });
 
-getUser(1, getRepositories);
-
 console.log("After");
-function getRepositories(user) {
-  getRepositories(user.githubUsername, getCommies);
-}
-
-function getCommies(repos) {
-  getCommies(repos, displayCommits);
-}
-
-function displayCommits(commits) {
-  console.log(commits);
-}
 
 function getUser(id, callback) {
   setTimeout(() => {
@@ -27,9 +18,57 @@ function getUser(id, callback) {
   }, 2000);
 }
 
-function getRepositories(username, callback) {
+function getRepositories(username) {
   setTimeout(() => {
     console.log(`Getting ${username}'s repositories...`);
     callback(["repo1", "repo2", "repo3"]);
   }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log(`Getting ${repo}'s commits...`);
+    callback(["commit"]);
+  }, 2000);
+}
+
+// async with Promises:
+
+console.log("Before");
+
+getUser(1).then((user) => {
+  getRepositories(user.githubUsername).then((repos) => {
+    getCommits(repos[0]).then((commits) => {
+      console.log(commits);
+    });
+  });
+});
+
+console.log("After");
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a use from a database...");
+      resolve({ id: id, githubUsername: "zahrabayatt" });
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Getting ${username}'s repositories...`);
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Getting ${repo}'s commits...`);
+      resolve(["commit"]);
+    }, 2000);
+  });
 }
