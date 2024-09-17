8fcfe35e38d1d359687a44a837df6da0c493989f
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-17 22:22:08 +0330

    Introducing MongoDB

--- vidly/index.js
+++ vidly/index.js
@@ -7,3 +7,11 @@ app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// So far we have stored the list of genre in an array in memory, this not how we build real world application because when server restarted we are going to lose all data in the memory, so that's why we need to store all data in the database.
+
+// What is MOngoDB?
+// https://www.mongodb.com/
+// https://www.w3schools.com/mongodb/
+
+// MongoDB is a document database or no-sql database. It's different form traditional relational databases like sql server or my sql,.. so in MongoDB we don't have concepts like tables, schema, views, record, column,... so unlike relational databases where you have to design your database ahead of time, in MongoDB there is no such thing like that and you simply store your json object in MongoDB. this also means when we querying MongoDB database we got a json object and we simply return that object back to the client and there is no transformation.
