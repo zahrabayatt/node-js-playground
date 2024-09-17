aaad26e0383fe902c2b8516ee7c0790d18521e00
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-18 01:21:15 +0330

    Installing MongoDB on Windows

--- vidly/index.js
+++ vidly/index.js
@@ -8,10 +8,13 @@ app.use("/api/genres", genres);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-// So far we have stored the list of genre in an array in memory, this not how we build real world application because when server restarted we are going to lose all data in the memory, so that's why we need to store all data in the database.
+// Download MongoDB: https://www.mongodb.com/try/download/community
+// install MongoDB Compass
 
-// What is MOngoDB?
-// https://www.mongodb.com/
-// https://www.w3schools.com/mongodb/
+// find the path of mongod : C:\Program Files\MongoDB\Server\7.0\bin add it as path in system variable in environment variables.
 
-// MongoDB is a document database or no-sql database. It's different form traditional relational databases like sql server or my sql,.. so in MongoDB we don't have concepts like tables, schema, views, record, column,... so unlike relational databases where you have to design your database ahead of time, in MongoDB there is no such thing like that and you simply store your json object in MongoDB. this also means when we querying MongoDB database we got a json object and we simply return that object back to the client and there is no transformation.
+// run mongod command in cmd to check if installed successfully. if you see this error: "NonExistentPath: Data directory C:\\data\\db\\ not found. Create the missing directory or specify another path using, run this command md c:\data\db
+// mongod
+// md c:\data\db
+
+// open MongoDB Compass, create a localhost connection with default config.
