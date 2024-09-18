927343b56c1c3bc191a5e8d200e437c0e7286014
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-18 19:19:21 +0330

    Mongoose Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,24 +1,35 @@
 const mongoose = require("mongoose");
 
-// Mongoose is a node package that gives us simple api to work with MongoDB database.
-
-// how to connect to mongodb?
-// we pass a connection string as input
 mongoose
   .connect("mongodb://127.0.0.1:27017/playground")
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err)); // use debug module to log in real time application
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-//The error you're encountering, MongooseServerSelectionError: connect ECONNREFUSED ::1:27017, suggests that Mongoose is trying to connect to MongoDB using the IPv6 address ::1 (localhost in IPv6), but the connection is being refused. This could be due to MongoDB not listening on the IPv6 address or not being properly configured.
-// Here are a few steps you can try to resolve this:
+// a collection in mongodb is like a table in relational database.
+// a document in mongodb is like record/row in relational database.
 
-// 1. Use IPv4 explicitly
-// You can force Mongoose to connect using the IPv4 version of localhost(127.0.0.1) instead of relying on localhost, which may resolve to the IPv6 address(:: 1).
+// in mongoose we have a concept called schema, this is just specify to mongoose and it's not part of mongodb. we use schema in mongoose to define a shape of the document in mongodb collection, so we use it to define what are the properties we have in the document of mongodb collection.
 
-// 2. Ensure MongoDB is running
-// You can check if MongoDB is running using the following steps:
-// Press Windows + R, type services.msc, and hit Enter.
-// In the Services window, scroll down to find MongoDB.
-// Ensure the service is running. If not, right-click on MongoDB and select Start.
-// Alternatively, you can open Command Prompt or PowerShell as Administrator and start MongoDB manually:
-// net start MongoDB
+// How to create a schema?
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String], // technically tags in database are pairs of key values where a key is index and the value is string
+  date: { type: Date, default: Date.now }, // we can specify the default value
+});
+
+// Schema types in Mongoose:
+// https://mongoosejs.com/docs/schematypes.html
+// String
+// Number
+// Date
+// Buffer - use for storing binary data
+// Boolean
+// ObjectId - use for assigning uniq identifier
+// Array
+// Decimal128
+// Map
+// Schema
+// UUID
+// BigInt
