959045f4a4f50b48f5fc609d5af165e0cf4f22b3
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-19 01:26:54 +0330

    Saving a Document in MongoDB

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -12,18 +12,29 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
 });
 
-// in js we can have a class and also have a instance of class which is called object. in this application we want to have a class called Course and a instance of Course called nodeCourse which is object and then we can save that nodeCourse in the database.
-// To create a class like Course, we need to compile the courseSchema to a model, this is how we do it:
+const Course = mongoose.model("Course", courseSchema);
 
-// the first argument is a singular name of collection that is model is for and the second argument is the schema that defined the shape of the document in this collection.
-const Course = mongoose.model("Course", courseSchema); // Course is a class! we use upper case for class names.
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Zahra Bayat",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
 
-// now we can create a object base on this class:
-const course = new Course({
-  name: "Node.js Course",
-  author: "Zahra Bayat",
-  tags: ["node", "backend"],
-  isPublished: true,
-}); // course is a object! we use pascal case for object names.
+  // save is a async function
+  // result is a course that is saved in database and mongodb assign a uniq id to it
+  const result = await course.save();
+  console.log(result);
+  // log:
+  //   {
+  //   name: 'Angular Course',
+  //   author: 'Zahra Bayat',
+  //   tags: [ 'angular', 'frontend' ],
+  //   _id: new ObjectId('66eb4c60ffeb176b160e4114'),
+  //   date: 2024-09-18T21:55:44.345Z,
+  //   __v: 0
+  // }
+}
 
-// in MongoDB or in no-sql databases general, a document can be a complex object like tags is a property of string, we don't have something like that in relational databases, in other words a row in relational databases has simple attributes, if you want to model this structure like course in relational database, you need three tables, Courses, Tags and the intermediary table called course tags because we have a many to many relationship between Courses and Tags. in no-sql databases like MongoDB we don't need this structures and we don't have to define these table and scripted, we simply carate our object and store them in a database. that's why we called them schemaless, they don't have schema.
+createCourse();
