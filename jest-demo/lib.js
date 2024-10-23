fb65ed489102a5e3c0e78156be6d817929d79625
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-23 19:16:09 +0330

    jest - Testing Numbers

---
+++ jest-demo/lib.js
@@ -0,0 +1,6 @@
+// Testing numbers
+module.exports.absolute = function (number) {
+  if (number > 0) return number;
+  if (number < 0) return -number;
+  return 0;
+};
--- jest-demo/tests/lib.test.js
+++ jest-demo/tests/lib.test.js
@@ -1,3 +1,17 @@
-test("Our first test", () => {
-  //throw new Error("something failed");
+const lib = require("../lib");
+
+test("absolute - should return a positive number if input is positive", () => {
+  const result = lib.absolute(1);
+  // matcher function in jest: https://jestjs.io/docs/using-matchers
+  expect(result).toBe(1);
+});
+
+test("absolute - should return a positive number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return 0 number if input is 0", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
 });
