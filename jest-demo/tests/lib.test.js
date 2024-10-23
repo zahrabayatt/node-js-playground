12b58b40a7461557d0ca4c12fe22d556963e80ce
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-23 19:19:10 +0330

    jest - Grouping Tests

--- jest-demo/tests/lib.test.js
+++ jest-demo/tests/lib.test.js
@@ -1,17 +1,20 @@
 const lib = require("../lib");
 
-test("absolute - should return a positive number if input is positive", () => {
-  const result = lib.absolute(1);
-  // matcher function in jest: https://jestjs.io/docs/using-matchers
-  expect(result).toBe(1);
-});
+// grouping related test using describe
+describe("absolute", () => {
+  // use it instead of test to make it more readable
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return a positive number if input is negative", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return 0 number if input is 0", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  it("should return 0 number if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
