313991fd8c679afb695e8e82d6690c0abee837f4
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-23 19:24:49 +0330

    jest - Refactoring with Confidence

--- jest-demo/lib.js
+++ jest-demo/lib.js
@@ -1,6 +1,5 @@
 // Testing numbers
 module.exports.absolute = function (number) {
-  if (number > 0) return number;
-  if (number < 0) return -number;
-  return 0;
+  return number >= 0 ? number : -number;
+};
 };
